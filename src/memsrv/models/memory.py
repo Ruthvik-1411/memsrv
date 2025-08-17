f913329211b10bbb7ff2767733b7cc5b92e02566
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2025-08-17 21:42:34 +0530

    chore: response and data model definition

--- src/memsrv/models/memory.py
+++ src/memsrv/models/memory.py
@@ -1,1 +1,21 @@
 """data models for facts, memories and api services will be added here"""
+from pydantic import BaseModel
+from typing import Optional, Dict, List, Any
+
+# TODO: add timestamps as well
+
+class MemoryMetadata(BaseModel):
+    user_id: str
+    app_id: str
+    session_id: str
+    timestamp: Optional[str] = ""
+    agent_name: str
+
+# TODO: update with more fields as per design
+class MemoryItem(BaseModel):
+    fact_id: str
+    fact: str
+    metadata: Dict[str, Any]
+
+class GetMemoriesResponseModel(BaseModel):
+    facts: List[MemoryItem]
