34cf003a049d3975c4841a064b73557042e2b406
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2025-08-23 22:02:51 +0530

    chore: add embedding generation modules

---
+++ src/memsrv/embeddings/base_embedder.py
@@ -0,0 +1,11 @@
+"""Base class for embeddings"""
+from abc import ABC, abstractmethod
+from typing import List
+
+class BaseEmbedding(ABC):
+    """Abstract interface for any embedding model provider."""
+
+    @abstractmethod
+    def generate_embeddings(self, texts: List[str]) -> List[List[float]]:
+        """Generates embeddings for a list of texts."""
+        pass
---
+++ src/memsrv/embeddings/providers/gemini.py
@@ -0,0 +1,32 @@
+"""Embeddings generator using google embeds"""
+import os
+from typing import List
+from google.genai.client import Client as geminiClient
+from google.genai.types import EmbedContentConfig
+from memsrv.embeddings.base_embedder import BaseEmbedding
+
+class GeminiEmbedding(BaseEmbedding):
+    """Embedding module for generating embeddings using gemini api"""
+    def __init__(self, model_name: str = "gemini-embedding-001", api_key: str = None):
+        self.embedding_model_name = model_name
+        api_key = api_key or os.getenv("GOOGLE_API_KEY")
+        self.client = geminiClient(api_key=api_key)
+
+    def generate_embeddings(self, texts: List[str]) -> List[List[float]]:
+        """Generates embeddings for a list of texts using Gemini embedding models."""
+        try:
+            embedding_result = []
+            result = self.client.models.embed_content(
+                model=self.embedding_model_name,
+                contents=texts,
+                config=EmbedContentConfig(
+                    task_type="RETRIEVAL_DOCUMENT",
+                    output_dimensionality=768
+                )
+            )
+            for embedding in result.embeddings:
+                embedding_result.append(embedding.values)
+            return embedding_result
+        except Exception as e:
+            print(f"An error occurred during embedding generation: {e}")
+            return [[] for _ in texts]
