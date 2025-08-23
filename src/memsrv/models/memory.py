2de99312b357637acce8c72b8bb932dddbe4d230
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2025-08-23 22:04:04 +0530

    chore: update data models

--- src/memsrv/models/memory.py
+++ src/memsrv/models/memory.py
@@ -1,5 +1,6 @@
 """data models for facts, memories and api services will be added here"""
-from pydantic import BaseModel
+import uuid
+from pydantic import BaseModel, Field
 from typing import Optional, Dict, List, Any
 
 # TODO: add timestamps as well
@@ -11,11 +12,18 @@ class MemoryMetadata(BaseModel):
     timestamp: Optional[str] = ""
     agent_name: str
 
+class DBMemoryItem(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    document: str
+    embedding: List[float]
+    metadata: MemoryMetadata
+
 # TODO: update with more fields as per design
 class MemoryItem(BaseModel):
     fact_id: str
     fact: str
     metadata: Dict[str, Any]
+    similarity: Optional[float]
 
 class GetMemoriesResponseModel(BaseModel):
     facts: List[MemoryItem]
