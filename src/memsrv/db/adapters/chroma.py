03594e8d5ff68904bfe4a4800998d36df332d7b2
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2025-08-27 20:06:47 +0530

    feat: add update,del for chroma db

--- src/memsrv/db/adapters/chroma.py
+++ src/memsrv/db/adapters/chroma.py
@@ -8,6 +8,15 @@ class ChromaDBAdapter(VectorDBAdapter):
     """Implements vector db ops for chroma DB"""
     def __init__(self, persist_dir: str = "./chroma_db"):
         self.client = chromadb.PersistentClient(path=persist_dir)
+        # Create a collection with default setting
+        # If there is a need to change the name, comment this and
+        # call this method when initializing the adapter directly
+        self.create_collection(
+            name="memories",
+            metadata={
+                "description": "Collection for memory service"
+            }
+        )
     
     def _format_filters(self, filters: Dict[str, Any]) -> Dict[str, Any]:
         """
@@ -26,30 +35,43 @@ class ChromaDBAdapter(VectorDBAdapter):
         else:
             return filters
 
-    def create_collection(self, name: str, metadata: Dict[str, Any] = None):
+    def create_collection(self, name, metadata):
         """Create a chroma collection"""
         # We should use this when we want to fix and always use the same collection
         # In some cases we might create them based on request params, this will help
         # TODO: Change default configuration of L2 to cosine
-        return self.client.get_or_create_collection(name=name, metadata=metadata)
+        self.client.get_or_create_collection(name=name, metadata=metadata)
+        return True
 
-    def add(self, collection_name: str, items: List[DBMemoryItem]):
-        """Adds items to chromaDB.
-        Args:
-            List of items with keys id, document, embeddings, metadata(dict)
-        """
-        # TODO: Change default configuration of L2 to cosine
-        collection = self.client.get_or_create_collection(name=collection_name)
+    def add(self, collection_name, items):
+        collection = self.client.get_collection(name=collection_name)
         collection.add(
             ids=[item.id for item in items],
             documents=[item.document for item in items],
             embeddings=[item.embedding for item in items],
-            metadatas=[item.metadata.dict() for item in items]
+            metadatas=[item.metadata.model_dump() for item in items]
         )
         print(f"Successfully added {len(items)} items to chroma collection.")
+        return [item.id for item in items]
+    
+    def update(self, collection_name, items):
+        collection = self.client.get_collection(name=collection_name)
+        collection.update(
+            ids=[item.id for item in items],
+            documents=[item.document for item in items],
+            embeddings=[item.embedding for item in items],
+            metadatas=[item.metadata.model_dump() for item in items]
+        )
+        print(f"Successfully updated {len(items)} items to chroma collection.")
+        return [item.id for item in items]
 
-    def query_by_filter(self, collection_name: str, filters: Dict[str, Any], limit = 20):
-        """Retrieve by metadata filters only"""
+    def delete(self, collection_name, fact_ids):
+        collection = self.client.get_collection(name=collection_name)
+        collection.delete(ids=fact_ids)
+        print(f"Successfully deleted memory with id {fact_ids} from chroma collection")
+        return fact_ids
+
+    def query_by_filter(self, collection_name, filters, limit):
         collection = self.client.get_collection(name=collection_name)
         where_clause = self._format_filters(filters)
         results = collection.get(
@@ -58,14 +80,7 @@ class ChromaDBAdapter(VectorDBAdapter):
         )
         return results
 
-    def query_by_similarity(
-        self, 
-        collection_name: str,
-        query_embedding: List[float],
-        query_text: Optional[str] = None,
-        filters: Optional[Dict[str, Any]] = None, 
-        top_k: int = 5
-    ):
+    def query_by_similarity(self, collection_name, query_embedding, query_text, filters, top_k):
         """Retreive items similar to query with optional filters"""
 
         collection = self.client.get_collection(name=collection_name)
