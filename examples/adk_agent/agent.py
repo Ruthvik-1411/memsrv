afe26497f2e3b7ab7aed88a2190b51a054f7e9a4
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2025-09-07 23:33:11 +0530

    chore: add create memories as a tool to adk_agent

--- examples/adk_agent/agent.py
+++ examples/adk_agent/agent.py
@@ -1,12 +1,41 @@
 """Core module for agent orchestration"""
+from typing import List
 from google.adk.agents import Agent
-
+from google.adk.tools import ToolContext
 from adk_agent.tools import get_current_time, calculate_expression
 from adk_agent.custom_memory_tool import CustomMemoryTool
 
 from dotenv import load_dotenv
+
+from shared.memory_client import MemoryClient
 # To load the google api keys
 load_dotenv()
+
+memory_client = MemoryClient()
+
+def save_memories(memories: List[str], tool_context: ToolContext):
+    """Add key memories to the memory service.
+    Args:
+        memories: list of memory content to be added to the database
+    """
+    user_id = tool_context._invocation_context.user_id
+    app_name = tool_context._invocation_context.app_name
+    session_id = tool_context._invocation_context.session.id
+
+    try:
+        response = memory_client.create_memory(memory_content=memories,
+                                               metadata={
+                                                   "user_id": user_id,
+                                                   "app_id": app_name,
+                                                   "session_id": session_id,
+                                                   "agent_name": app_name
+                                               })
+        print(response)
+        return {
+            "success": response
+        }
+    except Exception as e:
+        raise ValueError(e) from e
 
 root_agent = Agent(
     name="root_agent",
@@ -18,6 +47,7 @@ root_agent = Agent(
     use the appropriate tools to get the answer the user query.""",
     tools=[get_current_time,
            calculate_expression,
-           CustomMemoryTool(name="memory_tool", description="preload_memory")
+           CustomMemoryTool(name="memory_tool", description="preload_memory"),
+           save_memories
     ]
 )
--- examples/shared/memory_client.py
+++ examples/shared/memory_client.py
@@ -1,4 +1,5 @@
 """Common http client side implementation for memory service"""
+import os
 from typing import Dict, Any, List, Optional
 import requests
 
@@ -6,8 +7,16 @@ MEMORY_SERVICE_URL = "http://localhost:8090/api/v1"
 
 class MemoryClient:
     """Memory client that communicates with our memory service"""
-    def __init__(self, base_url: str = MEMORY_SERVICE_URL):
-        self.base_url = base_url.rstrip("/")
+    def __init__(self,
+                 base_url: Optional[str] = MEMORY_SERVICE_URL,
+                 user_id: Optional[str]=None,
+                 app_id: Optional[str]=None):
+        memory_service_url = base_url or os.getenv("MEMORY_SERVICE_URL")
+        self.base_url = memory_service_url.rstrip("/")
+        if user_id:
+            self.user_id = user_id
+        if app_id:
+            self.app_id = app_id
 
     def add_to_memory(self,
                       messages: List[Dict[str, Any]],
@@ -15,7 +24,7 @@ class MemoryClient:
         """
         Adds conversation messages + metadata to memory service.
         messages format: list of {role: str, content: str}, see events.json
-        metadata: {user_id, session_id, app_id, agent_name, timestamp(opt)}
+        metadata: {user_id, session_id, app_id, agent_name, event_timestamp(opt)}
         """
         url = f"{self.base_url}/memories/generate"
         payload = {"messages": messages, "metadata": metadata}
@@ -24,19 +33,59 @@ class MemoryClient:
         # TODO: Need to add better reading of errors
         response.raise_for_status()
         return response.json()
+    
+    def create_memory(self,
+                      memory_content: List[str],
+                      metadata: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Adds conversation messages + metadata to memory service.
+        messages format: list of {role: str, content: str}, see events.json
+        metadata: {user_id, session_id, app_id, agent_name, event_timestamp(opt)}
+        """
+        url = f"{self.base_url}/memories/create"
+        payload = {"documents": memory_content, "metadata": metadata}
+        print(payload)
+        response = requests.post(url, json=payload)
+        # TODO: Need to add better reading of errors
+        response.raise_for_status()
+        return response.json()
 
-    def get_memories(
-        self,
-        user_id: str,
-        session_id: Optional[str] = None,
-        app_id: Optional[str] = None,
-        limit: int = 50
-    ) -> Dict[str, Any]:
+    def get_memories(self,
+                     user_id: str,
+                     session_id: Optional[str] = None,
+                     app_id: Optional[str] = None,
+                     limit: int = 50) -> Dict[str, Any]:
         """
         Fetch memories filtered by metadata.
+        user_id, app_id are recommended to use always
         """
         url = f"{self.base_url}/memories"
         params = {
+            "user_id": user_id,
+            "session_id": session_id,
+            "app_id": app_id,
+            "limit": limit
+        }
+        response = requests.get(url,
+                                params={key: value for key, value in params.items()
+                                        if value is not None})
+        # TODO: Need to add better reading of errors
+        response.raise_for_status()
+        return response.json()
+    
+    def get_similar_memories(self,
+                             query: str,
+                             user_id: str,
+                             session_id: Optional[str] = None,
+                             app_id: Optional[str] = None,
+                             limit: int = 50) -> Dict[str, Any]:
+        """
+        Fetch similar memories filtered by metadata.
+        query, user_id and app_id are recommended to be sent always
+        """
+        url = f"{self.base_url}/memories/similar"
+        params = {
+            "query": query,
             "user_id": user_id,
             "session_id": session_id,
             "app_id": app_id,
